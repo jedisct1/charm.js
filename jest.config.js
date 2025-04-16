315f38025359296e3c54cf34fd08a43d985842ee
Frank Denis <github@pureftpd.org>
2025-04-16 12:41:55 +0200

    Fix Jest

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,16 @@
-module.exports = {
+export default {
     preset: 'ts-jest',
     testEnvironment: 'node',
     roots: ['<rootDir>/src'],
     testMatch: ['**/*.test.ts'],
     transform: {
-        '^.+\\.tsx?$': 'ts-jest',
+        '^.+\\.tsx?$': ['ts-jest', {
+            useESM: true
+        }]
     },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    extensionsToTreatAsEsm: ['.ts'],
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1'
+    }
 }; 
